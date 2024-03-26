67e91aa5d088c5c7123ad619234109308e598a64
mmackowsky <maciostah@gmail.com>
2024-03-26 17:49:19 +0100

    feat(main): update main file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
+import uvicorn
 from fastapi import FastAPI, Request, Response, status
 
+from config import get_settings
+
 app = FastAPI()
+settings = get_settings()
 
 
 @app.get("/api/users")
 async def read_users():
     return {"users": []}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=settings.service_host, port=settings.service_port)
