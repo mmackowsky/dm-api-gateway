4fe723ea34733b3d8dc9d29c0132c47e355c9c5d
mmackowsky <maciostah@gmail.com>
2024-03-26 17:45:47 +0100

    feat(config): update config file

--- src/config.py
+++ src/config.py
@@ -1,7 +1,19 @@
 import os
+from functools import lru_cache
 
-from pydantic import BaseSettings
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    pass
+    service_port: int = os.getenv("SERVICE_PORT")
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
