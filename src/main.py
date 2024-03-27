f075b89227b4705f0d72b273c126b52e4af5cb93
mmackowsky <maciostah@gmail.com>
2024-03-27 20:16:17 +0100

    feat(main): update main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import requests
 import uvicorn
 from fastapi import FastAPI, Request, Response, status
 
@@ -9,7 +10,8 @@ settings = get_settings()
 
 @app.get("/api/users")
 async def read_users():
-    return {"users": []}
+    response = requests.get(url="http://127.0.0.1:8002")
+    return response.status_code
 
 
 if __name__ == "__main__":
