a947411dc3e8561aa6f6b92d820edd5aa8e4a4b6
mmackowsky <maciostah@gmail.com>
2024-04-02 19:37:53 +0200

    config: update

--- src/config.py
+++ src/config.py
@@ -10,7 +10,7 @@ load_dotenv()
 class Settings(BaseSettings):
     service_port: int = os.getenv("SERVICE_PORT")
     service_host: str = os.getenv("SERVICE_HOST")
-    users_url: str = os.getenv("USERS_URL")
+    users_service_url: str = os.getenv("USERS_SERVICE_URL")
 
     class Config:
         env_file = ".env"
