2d3ba8d6212622a91d07761cfbddb2ea732fa3a6
mmackowsky <maciostah@gmail.com>
2024-04-05 21:02:36 +0200

    config: refactor file

--- src/config.py
+++ src/config.py
@@ -8,11 +8,15 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    secret_key: str = os.getenv("SECRET_KEY")
-    algorithm: str = os.getenv("ALGORITHM")
-    service_port: int = os.getenv("SERVICE_PORT")
-    service_host: str = os.getenv("SERVICE_HOST")
-    users_service_url: str = os.getenv("USERS_SERVICE_URL")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: str = os.getenv("ALGORITHM")
+    SERVICE_PORT: int = os.getenv("SERVICE_PORT")
+    SERVICE_HOST: str = os.getenv("SERVICE_HOST")
+    USERS_SERVICE_URL: str = os.getenv("USERS_SERVICE_URL")
+    GATEWAY_TIMEOUT: int = os.getenv("GATEWAY_TIMEOUT")
+    ACCESS_TOKEN_DEFAULT_EXPIRE_MINUTES: int = os.getenv(
+        "ACCESS_TOKEN_DEFAULT_EXPIRE_MINUTES"
+    )
 
     class Config:
         env_file = ".env"
