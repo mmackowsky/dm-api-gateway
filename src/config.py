d18578c05f1a58bfe2076b4cc5983d333755d410
mmackowsky <maciostah@gmail.com>
2024-04-15 18:50:10 +0200

    config: update

--- src/config.py
+++ src/config.py
@@ -8,16 +8,23 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
-    ALGORITHM: str = os.getenv("ALGORITHM")
-    SERVICE_PORT: int = os.getenv("SERVICE_PORT")
-    SERVICE_HOST: str = os.getenv("SERVICE_HOST")
-    USERS_SERVICE_URL: str = os.getenv("USERS_SERVICE_URL")
-    ENERGY_SERVICE_URL: str = os.getenv("ENERGY_SERVICE_URL")
-    GATEWAY_TIMEOUT: int = os.getenv("GATEWAY_TIMEOUT")
+    """
+    API Gateway variables
+    """
+
     ACCESS_TOKEN_DEFAULT_EXPIRE_MINUTES: int = os.getenv(
         "ACCESS_TOKEN_DEFAULT_EXPIRE_MINUTES"
     )
+    ALGORITHM: str = os.getenv("ALGORITHM")
+    GATEWAY_TIMEOUT: int = os.getenv("GATEWAY_TIMEOUT")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    SERVICE_PORT: int = os.getenv("SERVICE_PORT")
+    SERVICE_HOST: str = os.getenv("SERVICE_HOST")
+    """
+    External services urls.
+    """
+    USERS_SERVICE_URL: str = os.getenv("USERS_SERVICE_URL")
+    ENERGY_SERVICE_URL: str = os.getenv("ENERGY_SERVICE_URL")
 
     class Config:
         env_file = ".env"
