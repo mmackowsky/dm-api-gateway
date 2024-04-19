228994ae1b093fe49c62053626cc1be70737940e
mmackowsky <maciostah@gmail.com>
2024-04-19 16:26:03 +0200

    feat(main): add new routes

--- src/main.py
+++ src/main.py
@@ -13,6 +13,10 @@ from decorator import route
 app = FastAPI()
 settings = get_settings()
 
+"""
+Users Service
+"""
+
 
 @route(
     request_method=app.post,
@@ -117,6 +121,11 @@ async def update_user(
     pass
 
 
+"""
+Energy Consumption Service
+"""
+
+
 @route(
     request_method=app.get,
     path="/api/energy",
@@ -130,14 +139,50 @@ async def update_user(
     service_header_generator="auth.generate_request_header",
     # response_model="datastructures.users.UserResponse",
 )
-async def get_measurements(request: Request, response: Response):
+async def get_energy_consumptions(request: Request, response: Response):
+    pass
+
+
+@route(
+    request_method=app.get,
+    path="/api/energy/{measurement_id}",
+    status_code=status.HTTP_200_OK,
+    payload_key=None,
+    service_url=settings.ENERGY_SERVICE_URL,
+    authentication_required=True,
+    post_processing_func=None,
+    authentication_token_decoder="auth.decode_access_token",
+    service_authorization_checker="auth.is_default_user",  # CHANGE FROM is_admin_user to is_default_user
+    service_header_generator="auth.generate_request_header",
+    # response_model="datastructures.users.UserResponse",
+)
+async def get_energy_consumption_by_id(
+    measurement_id: int, request: Request, response: Response
+):
+    pass
+
+
+@route(
+    request_method=app.delete,
+    path="/api/energy/{measurement_id}",
+    status_code=status.HTTP_200_OK,
+    payload_key=None,
+    service_url=settings.ENERGY_SERVICE_URL,
+    authentication_required=True,
+    post_processing_func=None,
+    authentication_token_decoder="auth.decode_access_token",
+    service_authorization_checker="auth.is_default_user",  # CHANGE FROM is_admin_user to is_default_user
+    service_header_generator="auth.generate_request_header",
+    # response_model="datastructures.users.UserResponse",
+)
+async def delete_measurement(measurement_id: int, request: Request, response: Response):
     pass
 
 
 @route(
     request_method=app.post,
     path="/api/fake-measurement",
-    status_code=status.HTTP_200_OK,
+    status_code=status.HTTP_201_CREATED,
     payload_key=None,
     service_url=settings.ENERGY_SERVICE_URL,
     authentication_required=True,
