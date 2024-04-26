79be7601e0bff03de0f40c0d03ee4a4d187f036a
mmackowsky <maciostah@gmail.com>
2024-04-26 17:36:22 +0200

    feat(main): add routes for devices service

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI, Request, Response, status
 
 from config import get_settings
+from datastructures.devices import DeviceForm
 from datastructures.users import (
     LoginResponse,
     UserForm,
@@ -317,6 +318,75 @@ async def get_payments(request: Request, response: Response):
     service_header_generator="auth.generate_request_header",
 )
 async def get_payments(id: int, request: Request, response: Response):
+    pass
+
+
+"""
+Devices Service
+"""
+
+
+@route(
+    request_method=app.post,
+    path="/api/devices/",
+    status_code=status.HTTP_201_CREATED,
+    payload_key=None,
+    service_url=settings.DEVICES_SERVICE_URL,
+    authentication_required=True,
+    post_processing_func=None,
+    authentication_token_decoder="auth.decode_access_token",
+    service_authorization_checker="auth.is_default_user",  # CHANGE FROM is_admin_user to is_default_user
+    service_header_generator="auth.generate_request_header",
+)
+async def add_device(device_form: DeviceForm, request: Request, response: Response):
+    pass
+
+
+@route(
+    request_method=app.get,
+    path="/api/devices/",
+    status_code=status.HTTP_200_OK,
+    payload_key=None,
+    service_url=settings.DEVICES_SERVICE_URL,
+    authentication_required=True,
+    post_processing_func=None,
+    authentication_token_decoder="auth.decode_access_token",
+    service_authorization_checker="auth.is_default_user",  # CHANGE FROM is_admin_user to is_default_user
+    service_header_generator="auth.generate_request_header",
+)
+async def get_devices(request: Request, response: Response):
+    pass
+
+
+@route(
+    request_method=app.get,
+    path="/api/devices/{id}",
+    status_code=status.HTTP_200_OK,
+    payload_key=None,
+    service_url=settings.DEVICES_SERVICE_URL,
+    authentication_required=True,
+    post_processing_func=None,
+    authentication_token_decoder="auth.decode_access_token",
+    service_authorization_checker="auth.is_default_user",  # CHANGE FROM is_admin_user to is_default_user
+    service_header_generator="auth.generate_request_header",
+)
+async def get_device_by_id(request: Request, response: Response, id: int):
+    pass
+
+
+@route(
+    request_method=app.delete,
+    path="/api/devices/{id}",
+    status_code=status.HTTP_200_OK,
+    payload_key=None,
+    service_url=settings.DEVICES_SERVICE_URL,
+    authentication_required=True,
+    post_processing_func=None,
+    authentication_token_decoder="auth.decode_access_token",
+    service_authorization_checker="auth.is_default_user",  # CHANGE FROM is_admin_user to is_default_user
+    service_header_generator="auth.generate_request_header",
+)
+async def delete_device(request: Request, response: Response, id: int):
     pass
 
 
