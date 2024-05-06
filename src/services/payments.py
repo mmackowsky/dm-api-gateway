f235e4565a1f7fd0cc514f17ea7139d11e87b364
mmackowsky <maciostah@gmail.com>
2024-05-06 20:20:52 +0200

    feat(payments): update routes

--- src/services/payments.py
+++ src/services/payments.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import Request, Response, status
+from fastapi import Header, Request, Response, status
 
 from src.decorator import route
 from src.main import app, settings
@@ -7,7 +7,7 @@ from src.main import app, settings
 
 @route(
     request_method=app.post,
-    path="/api/payment/",
+    path="/stripe/cancel",
     status_code=status.HTTP_200_OK,
     payload_key=None,
     service_url=settings.PAYMENTS_SERVICE_URL,
@@ -17,13 +17,13 @@ from src.main import app, settings
     service_authorization_checker="auth.is_default_user",  # CHANGE FROM is_admin_user to is_default_user
     service_header_generator="auth.generate_request_header",
 )
-async def process_payment(request: Request, response: Response):
+async def cancel(request: Request, response: Response):
     pass
 
 
 @route(
     request_method=app.get,
-    path="/api/payment/",
+    path="/stripe/success",
     status_code=status.HTTP_200_OK,
     payload_key=None,
     service_url=settings.PAYMENTS_SERVICE_URL,
@@ -33,13 +33,13 @@ async def process_payment(request: Request, response: Response):
     service_authorization_checker="auth.is_admin_user",  # CHANGE FROM is_admin_user to is_default_user
     service_header_generator="auth.generate_request_header",
 )
-async def get_payments(request: Request, response: Response):
+async def success(request: Request, response: Response):
     pass
 
 
 @route(
     request_method=app.get,
-    path="/api/payment/{id}",
+    path="/stripe/create-payment-session",
     status_code=status.HTTP_200_OK,
     payload_key=None,
     service_url=settings.PAYMENTS_SERVICE_URL,
@@ -49,5 +49,39 @@ async def get_payments(request: Request, response: Response):
     service_authorization_checker="auth.is_admin_user",  # CHANGE FROM is_admin_user to is_default_user
     service_header_generator="auth.generate_request_header",
 )
-async def get_payments(id: int, request: Request, response: Response):
+async def create_payment_session(id: int, request: Request, response: Response):
+    pass
+
+
+@route(
+    request_method=app.post,
+    path="/stripe/webhook",
+    status_code=status.HTTP_200_OK,
+    payload_key=None,
+    service_url=settings.PAYMENTS_SERVICE_URL,
+    authentication_required=True,
+    post_processing_func=None,
+    authentication_token_decoder="auth.decode_access_token",
+    service_authorization_checker="auth.is_admin_user",  # CHANGE FROM is_admin_user to is_default_user
+    service_header_generator="auth.generate_request_header",
+)
+async def webhook(
+    request: Request, response: Response, stripe_signature: str = Header(None)
+):
+    pass
+
+
+@route(
+    request_method=app.get,
+    path="/stripe/checkout",
+    status_code=status.HTTP_200_OK,
+    payload_key=None,
+    service_url=settings.PAYMENTS_SERVICE_URL,
+    authentication_required=True,
+    post_processing_func=None,
+    authentication_token_decoder="auth.decode_access_token",
+    service_authorization_checker="auth.is_admin_user",  # CHANGE FROM is_admin_user to is_default_user
+    service_header_generator="auth.generate_request_header",
+)
+async def checkout(request: Request, response: Response):
     pass
