53a05916619b595d8f733a4930c30b1e41a939ef
mmackowsky <maciostah@gmail.com>
2024-05-07 23:06:29 +0200

    feat(main): add new routes

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Request, Response, status
 
 from config import get_settings
 from datastructures.users import UsernamePasswordForm
-from services import energy_consumption, users
+from services import devices, energy_consumption, payments, users, water_consumption
 
 app = FastAPI()
 settings = get_settings()
@@ -11,6 +11,9 @@ settings = get_settings()
 
 app.include_router(users.router)
 app.include_router(energy_consumption.router)
+app.include_router(water_consumption.router)
+app.include_router(devices.router)
+app.include_router(payments.router)
 
 
 if __name__ == "__main__":
