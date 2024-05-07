3e33734dc97023a9a77d1b1cf4f39f08a8012378
mmackowsky <maciostah@gmail.com>
2024-05-07 23:07:08 +0200

    feat(devices): refactor file

--- src/services/devices.py
+++ src/services/devices.py
@@ -1,74 +1,53 @@
-import uvicorn
-from fastapi import FastAPI, Request, Response, status
+from fastapi import APIRouter, Request, Response, status
 
+from src.config import get_settings
 from src.datastructures.devices import DeviceForm
-from src.decorator import route
-from src.main import app, settings
+from src.handlers import auth_handler
+
+settings = get_settings()
+router = APIRouter(prefix="/api", tags=["Devices"])
 
 
-@route(
-    request_method=app.post,
-    path="/api/devices/",
-    status_code=status.HTTP_201_CREATED,
-    payload_key=None,
-    service_url=settings.DEVICES_SERVICE_URL,
-    authentication_required=True,
-    post_processing_func=None,
-    authentication_token_decoder="auth.decode_access_token",
-    service_authorization_checker="auth.is_default_user",  # CHANGE FROM is_admin_user to is_default_user
-    service_header_generator="auth.generate_request_header",
-)
+@router.post("/devices", status_code=status.HTTP_201_CREATED)
 async def add_device(device_form: DeviceForm, request: Request, response: Response):
-    pass
+    return await auth_handler(
+        request=request,
+        response=response,
+        service_url=settings.DEVICES_SERVICE_URL,
+        status_code=status.HTTP_201_CREATED,
+        authentication_required=True,
+        payload=device_form.dict(),
+    )
 
 
-@route(
-    request_method=app.get,
-    path="/api/devices/",
-    status_code=status.HTTP_200_OK,
-    payload_key=None,
-    service_url=settings.DEVICES_SERVICE_URL,
-    authentication_required=True,
-    post_processing_func=None,
-    authentication_token_decoder="auth.decode_access_token",
-    service_authorization_checker="auth.is_default_user",  # CHANGE FROM is_admin_user to is_default_user
-    service_header_generator="auth.generate_request_header",
-)
+@router.get("/devices", status_code=status.HTTP_200_OK)
 async def get_devices(request: Request, response: Response):
-    pass
+    return await auth_handler(
+        request=request,
+        response=response,
+        service_url=settings.DEVICES_SERVICE_URL,
+        status_code=status.HTTP_200_OK,
+        authentication_required=True,
+    )
 
 
-@route(
-    request_method=app.get,
-    path="/api/devices/{id}",
-    status_code=status.HTTP_200_OK,
-    payload_key=None,
-    service_url=settings.DEVICES_SERVICE_URL,
-    authentication_required=True,
-    post_processing_func=None,
-    authentication_token_decoder="auth.decode_access_token",
-    service_authorization_checker="auth.is_default_user",  # CHANGE FROM is_admin_user to is_default_user
-    service_header_generator="auth.generate_request_header",
-)
+@router.get("/devices/{id}", status_code=status.HTTP_200_OK)
 async def get_device_by_id(request: Request, response: Response, id: int):
-    pass
+    return await auth_handler(
+        request=request,
+        response=response,
+        service_url=settings.DEVICES_SERVICE_URL,
+        status_code=status.HTTP_200_OK,
+        authentication_required=True,
+    )
 
 
-@route(
-    request_method=app.delete,
-    path="/api/devices/{id}",
-    status_code=status.HTTP_200_OK,
-    payload_key=None,
-    service_url=settings.DEVICES_SERVICE_URL,
-    authentication_required=True,
-    post_processing_func=None,
-    authentication_token_decoder="auth.decode_access_token",
-    service_authorization_checker="auth.is_default_user",  # CHANGE FROM is_admin_user to is_default_user
-    service_header_generator="auth.generate_request_header",
-)
+@router.delete("/devices/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_device(request: Request, response: Response, id: int):
-    pass
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host=settings.SERVICE_HOST, port=settings.SERVICE_PORT)
+    return await auth_handler(
+        request=request,
+        response=response,
+        service_url=settings.DEVICES_SERVICE_URL,
+        status_code=status.HTTP_204_NO_CONTENT,
+        authentication_required=True,
+    )
