7f3dd3e9e194adc572aad289895079e20648a4f9
mmackowsky <maciostah@gmail.com>
2024-05-07 23:08:12 +0200

    feat(payments): refactor file

--- src/services/payments.py
+++ src/services/payments.py
@@ -1,87 +1,64 @@
-import uvicorn
-from fastapi import Header, Request, Response, status
+from fastapi import APIRouter, Header, Request, Response, status
 
-from src.decorator import route
-from src.main import app, settings
+from src.config import get_settings
+from src.handlers import auth_handler
+
+router = APIRouter(prefix="/api", tags=["Payments"])
+settings = get_settings()
 
 
-@route(
-    request_method=app.post,
-    path="/stripe/cancel",
-    status_code=status.HTTP_200_OK,
-    payload_key=None,
-    service_url=settings.PAYMENTS_SERVICE_URL,
-    authentication_required=True,
-    post_processing_func=None,
-    authentication_token_decoder="auth.decode_access_token",
-    service_authorization_checker="auth.is_default_user",  # CHANGE FROM is_admin_user to is_default_user
-    service_header_generator="auth.generate_request_header",
-)
+@router.get("/stripe/cancel", status_code=status.HTTP_200_OK)
 async def cancel(request: Request, response: Response):
-    pass
+    return await auth_handler(
+        request=request,
+        response=response,
+        status_code=status.HTTP_200_OK,
+        authentication_required=True,
+        service_url=settings.PAYMENTS_SERVICE_URL,
+    )
 
 
-@route(
-    request_method=app.get,
-    path="/stripe/success",
-    status_code=status.HTTP_200_OK,
-    payload_key=None,
-    service_url=settings.PAYMENTS_SERVICE_URL,
-    authentication_required=True,
-    post_processing_func=None,
-    authentication_token_decoder="auth.decode_access_token",
-    service_authorization_checker="auth.is_admin_user",  # CHANGE FROM is_admin_user to is_default_user
-    service_header_generator="auth.generate_request_header",
-)
+@router.get("/stripe/success", status_code=status.HTTP_200_OK)
 async def success(request: Request, response: Response):
-    pass
+    return await auth_handler(
+        request=request,
+        response=response,
+        status_code=status.HTTP_200_OK,
+        authentication_required=True,
+        service_url=settings.PAYMENTS_SERVICE_URL,
+    )
 
 
-@route(
-    request_method=app.get,
-    path="/stripe/create-payment-session",
-    status_code=status.HTTP_200_OK,
-    payload_key=None,
-    service_url=settings.PAYMENTS_SERVICE_URL,
-    authentication_required=True,
-    post_processing_func=None,
-    authentication_token_decoder="auth.decode_access_token",
-    service_authorization_checker="auth.is_admin_user",  # CHANGE FROM is_admin_user to is_default_user
-    service_header_generator="auth.generate_request_header",
-)
-async def create_payment_session(id: int, request: Request, response: Response):
-    pass
+@router.get("/stripe/create-payment-session", status_code=status.HTTP_200_OK)
+async def create_payment_session(request: Request, response: Response):
+    return await auth_handler(
+        request=request,
+        response=response,
+        status_code=status.HTTP_200_OK,
+        authentication_required=True,
+        service_url=settings.PAYMENTS_SERVICE_URL,
+    )
 
 
-@route(
-    request_method=app.post,
-    path="/stripe/webhook",
-    status_code=status.HTTP_200_OK,
-    payload_key=None,
-    service_url=settings.PAYMENTS_SERVICE_URL,
-    authentication_required=True,
-    post_processing_func=None,
-    authentication_token_decoder="auth.decode_access_token",
-    service_authorization_checker="auth.is_admin_user",  # CHANGE FROM is_admin_user to is_default_user
-    service_header_generator="auth.generate_request_header",
-)
+@router.post("/stripe/webhook", status_code=status.HTTP_201_CREATED)
 async def webhook(
     request: Request, response: Response, stripe_signature: str = Header(None)
 ):
-    pass
+    return await auth_handler(
+        request=request,
+        response=response,
+        status_code=status.HTTP_201_CREATED,
+        authentication_required=True,
+        service_url=settings.PAYMENTS_SERVICE_URL,
+    )
 
 
-@route(
-    request_method=app.get,
-    path="/stripe/checkout",
-    status_code=status.HTTP_200_OK,
-    payload_key=None,
-    service_url=settings.PAYMENTS_SERVICE_URL,
-    authentication_required=True,
-    post_processing_func=None,
-    authentication_token_decoder="auth.decode_access_token",
-    service_authorization_checker="auth.is_admin_user",  # CHANGE FROM is_admin_user to is_default_user
-    service_header_generator="auth.generate_request_header",
-)
-async def checkout(request: Request, response: Response):
-    pass
+@router.get("/stripe/checkout", status_code=status.HTTP_200_OK)
+async def create_payment_session(request: Request, response: Response):
+    return await auth_handler(
+        request=request,
+        response=response,
+        status_code=status.HTTP_200_OK,
+        authentication_required=True,
+        service_url=settings.PAYMENTS_SERVICE_URL,
+    )
